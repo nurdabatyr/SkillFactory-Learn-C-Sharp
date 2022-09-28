41ac8ba0728eb872012a6b9461f2e90a2147d28e
Nurdaulet Batyrbek <nurdamarket@gmail.com>
2022-09-28 15:15:21 +0600

    Добавьте файлы проекта.

---
+++ SkillFactory Learn C Sharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillFactory_Learn_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
